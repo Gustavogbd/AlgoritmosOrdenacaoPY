8c87de2303ef23abafd4062d513dcf7f4f5aad0e
Gustavogbd <gustavokkg2@hotmail.com>
2023-06-26 18:08:44 -0300

    Refactor Bubble Sort

--- bubble_sortPY/bubble_sortPython.py
+++ bubble_sortPY/bubble_sortPython.py
@@ -1,17 +1,17 @@
-minha_lista = []
-troca = True #'Switch' para sair ou se manter no while
-numero = int(input("Quantidade de números da sua Lista: "))
+def bubbleSort(array):
+  
+  for i in range(len(array)):
 
-for i in range(numero): # Insere cada número da lista do usuário dentro da minha_lista
-    valor = int(input("Insira os números da sua lista, um de cada vez: "))
-    minha_lista.append(valor)
+    for j in range(0, len(array) - i - 1):
+      if array[j] > array[j + 1]:
+        temp = array[j]
+        array[j] = array[j+1]
+        array[j+1] = temp
 
-#Loop que implementa o Bubble Sort
-while troca:
-    troca = False
-    for i in range(len(minha_lista) - 1):#Percorre os números da lista, e a cada iteração, compara com o número subsequente
-        if minha_lista[i] > minha_lista[i + 1]:
-            minha_lista [i], minha_lista[i + 1] = minha_lista[i+ 1], minha_lista[i] #Realiza a troca se o número selecionado for maior
-            troca = True
 
-print("Sua lista ordenada: ", minha_lista)
+data = [-2, 45, 0, 11, -9]
+
+bubbleSort(data)
+
+print('Array ordenado com Bubble Sort')
+print(data)
